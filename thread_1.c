036942c00b8bcfe9819f5ef8aea30bc875876125
Vicente Bartual <vicentebartualf@gmail.com>
2021-06-23 15:49:18 +0200

    Add print at beginning thread 1

--- thread_1.c
+++ thread_1.c
@@ -34,6 +34,10 @@ static void print_flag_cb(void)
 /** Public functions -------------------------------------------------------- */
 void* thread_1_main(void* args)
 {
+	pthread_mutex_lock(&print_mtx);
+	printf("Thread 1\n");
+	pthread_mutex_unlock(&print_mtx);
+
 	angles_init();
 	timer_init();
 	app_timer_t* gyro_timer = create_timer(read_gyro_flag_cb, 500, 50);
