c1e1159bd355d7fef6eddc8eeef0101c379113c0
Vicente Bartual <vicentebartualf@gmail.com>
2021-06-23 15:43:02 +0200

    Join threads at the end

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 
 #include "main.h"
 
+/** Static variables -------------------------------------------------------- */
 pthread_mutex_t print_mtx = PTHREAD_MUTEX_INITIALIZER;
 
+/** Main function ----------------------------------------------------------- */
 int main(void)
 {
 	pthread_mutex_lock(&print_mtx);
@@ -12,11 +14,14 @@ int main(void)
 	pthread_mutex_unlock(&print_mtx);
 
 	pthread_t thread1, thread2, thread3;
-	//pthread_create(&thread1, NULL, thread_1_main, NULL);
+	pthread_create(&thread1, NULL, thread_1_main, NULL);
 	pthread_create(&thread2, NULL, thread_2_main, NULL);
-	//pthread_create(&thread3, NULL, thread_3_main, NULL);
+	pthread_create(&thread3, NULL, thread_3_main, NULL);
 	
 	thread_0_main(NULL);
 
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	pthread_join(thread3, NULL);
 	return 0;
 }
