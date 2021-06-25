3b85bcf2c906789686a0f931f4998e2174d83384
Vicente Bartual <vicentebartualf@gmail.com>
2021-06-25 14:51:02 +0200

    FIx pid memory addresses

--- lib/pid.h
+++ lib/pid.h
@@ -1,7 +1,18 @@
 #ifndef __PID_H
 #define __PID_H
 
-typedef struct pid_instance_t* pid_ints_t;
+typedef struct pid_instance_t {
+	double Kp;
+	double Ki;
+	double Kd;
+	double Kr;
+	double Kf;
+	double Ki_accum;
+	double Kd_last;
+	double Kr_accum;
+	double Kf_last;
+	uint64_t last_time;
+} pid_ints_t;
 
 void pid_create(pid_ints_t pid, double kp, double ki, double kd, double kr,
 		double kf);
